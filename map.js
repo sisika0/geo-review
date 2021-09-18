da3d268a6a08c7c5e4057a87f0f41db928b24f9f
Aleksandr Luks <senior.a.lux@yandex.ru>
2021-09-18 12:09:37 +0300

    Update slider and geolocations

--- map.js
+++ map.js
@@ -1,28 +1,50 @@
 let storage = localStorage;
 
 let formLayout = `
-	<ul class="reviews" id="review-list"></ul>
-	<div class="form" id="review-form">
-		<h3 class="form__title">Отзыв:</h3>
-		<input type="text" id="review-name" class="form__input" placeholder="Укажите ваше имя"/>
-		<input type="text" id="review-place" class="form__input" placeholder="Укажите место"/>
-		<textarea id="review-text" class="form__input form__input_textarea" placeholder="Оставьте отзыв"></textarea>
-		<button class="form__btn" id="add-button">Добавить</button>
-	</div>
+  <div class="form" id="review-form">
+    <h3 class="form__title">Отзыв:</h3>
+    <input type="text" id="review-name" class="form__input" placeholder="Укажите ваше имя"/>
+    <input type="text" id="review-place" class="form__input" placeholder="Укажите место"/>
+    <textarea id="review-text" class="form__input form__input_textarea" placeholder="Оставьте отзыв"></textarea>
+    <button class="form__btn" id="add-button">Добавить</button>
+    <span class="form__error" id="error">Заполните все поля!</span>
+  </div>
+`;
+let reviewsLayout = `
+  <div class="slider">
+    <button class="arrow prev" id="prev"></button>
+    <div class="slider__container">
+      <ul class="reviews" id="review-list"></ul>
+    </div>
+    <button class="arrow next" id="next"></button>
+  </div>
 `;
 
 let init = () => {
-	let currentCoords = [];
+  let currentCoords = [];
 
 	let yMap = new ymaps.Map("map", {
 		center: [59.935, 30.325],
 		zoom: 15
 	},
 	{
-		yandexMapDisablePoiInteractivity: true
+		yandexMapDisablePoiInteractivity: true  
 	});
 
+  let myIconContentLayout = ymaps.templateLayoutFactory.createClass(
+    `<div>
+      <div style="font-size: 20px; margin: -9px auto;">{{ properties.geoObjects.length }}</div>
+    </div>`
+  );
+
 	let yClusterer = new ymaps.Clusterer({
+    clusterIcons: [
+      {
+          href: './assets/1-review.png',
+          size: [70, 70],
+          offset: [-35, -70]
+      }],
+    clusterIconContentLayout: myIconContentLayout,
 		groupByCoordinates: true,
 		clusterDisableClickZoom: true,
 		clusterOpenBalloonOnClick: false,
@@ -34,7 +56,6 @@ let init = () => {
 		storageContent.push(newReview);
 
 		storage.reviews = JSON.stringify(storageContent);
-		console.log(storage);
 	};
 
 	let getReviewsFromStorage = (coordsOfBalloon) => {
@@ -65,43 +86,59 @@ let init = () => {
 		let reviews = getReviewsFromStorage(coords);
 		let form = createForm(coords, reviews);
 		
-		yMap.balloon.open(coords, form.innerHTML)
+		yMap.balloon.open(coords, form.innerHTML, {
+      maxHeight: 900
+    });
 	};
 
 	let createPlacemark = (coords) => {
-		let placemark = new ymaps.Placemark(coords);
-
-		placemark.events.add('click', e => {
-			let placemarkCoords = e.get('target').geometry.getCoordinates();
-
-			fillBalloon(placemarkCoords);
-		});
+		let placemark = new ymaps.Placemark(coords, {}, 
+    {
+      iconLayout: 'default#image',
+      iconImageHref: './assets/1-review.png',
+      iconImageSize: [70, 70],
+      iconImageOffset: [-35, -70]
+    });
 
 		yClusterer.add(placemark);
 	};
 
 	let createForm = (coords, reviews) => {
+    currentCoords = coords;
+
 		let formWrapper = document.createElement('div');
 
-		formWrapper.innerHTML = formLayout;
+    if (reviews.length === 0) {
+      formWrapper.innerHTML = formLayout;
 
-		let reviewList = formWrapper.querySelector('#review-list');
+      return formWrapper
+    }
 
-		currentCoords = coords;
+    formWrapper.innerHTML = reviewsLayout + formLayout;
+
+    let reviewList = formWrapper.querySelector('#review-list');
+    let nextArrow = formWrapper.querySelector('#next');
 
 		reviews.forEach(elem => {
 			let reviewsItem = document.createElement('li');
 
 			reviewsItem.classList.add('reviews__item');
 			reviewsItem.innerHTML = `
-				<div>
-					<b>${elem.name}</b> [${elem.place}]
-				</div>
-				<div>${elem.text}</div>
+        <div class="review__header">
+          <span class="review__name">${elem.name}</span>
+          <span class="review__place"> про ${elem.place}</span>
+        </div>
+				<blockquote class="review__text">
+          <q>${elem.text}</q>
+        </blockquote>
 			`;
 
 			reviewList.appendChild(reviewsItem);
 		});
+
+    if (reviewList.children.length > 1) {
+      nextArrow.classList.add('active')
+    }
 
 		return formWrapper;
 	};
@@ -128,23 +165,67 @@ let init = () => {
 
 	yMap.geoObjects.add(yClusterer);
 
+  let status = true;
+
 	document.addEventListener('click', e => {
 		if (e.target.id === 'add-button') {
 			let coords = currentCoords;
+      let name = document.querySelector('#review-name').value;
+      let place = document.querySelector('#review-place').value;
+      let text = document.querySelector('#review-text').value;
+      let error = document.querySelector('#error');
 
 			let newReview = {
 				coords,
-				review: {
-					name: document.querySelector('#review-name').value,
-					place: document.querySelector('#review-place').value,
-					text: document.querySelector('#review-text').value,
-				},
+				review: { name, place, text },
 			};
 
-			addReviewToStorage(newReview);
-			createPlacemark(coords);
-			yMap.balloon.close();
+      if (!name || !place || !text) {
+        error.style.display = 'inline'
+      } else {
+        addReviewToStorage(newReview);
+        createPlacemark(coords);
+        yMap.balloon.close(); 
+      }
 		}
+
+    let reviewList = document.querySelector('#review-list');
+    let nextArrow = document.querySelector('#next');
+    let prevArrow = document.querySelector('#prev');
+
+    if (e.target.id === 'next') {
+      let curPos = getComputedStyle(reviewList).transform.split(',')[4];
+      
+      reviewList.addEventListener('transitionend', () => {
+        status = true;
+      }, {once: true})
+
+      if (Math.abs(curPos) / 250 === reviewList.children.length - 2) {
+        nextArrow.style.visibility = 'hidden'
+      }
+      if (status) {
+        reviewList.style.transform = `translateX(${+curPos - 250}px)`;
+        prevArrow.style.visibility = 'visible';
+        status = false;
+      }
+    }
+
+    if (e.target.id === 'prev') {
+      let curPos = getComputedStyle(reviewList).transform.split(',')[4];
+
+      reviewList.addEventListener('transitionend', () => {
+        status = true;
+      }, {once: true})
+
+      if (Math.abs(curPos) / 250 === 1) {
+        prevArrow.style.visibility = 'hidden'
+      }
+      if (status) {
+        reviewList.style.transform = `translateX(${+curPos + 250}px)`;
+        nextArrow.style.visibility = 'visible';
+        status = false;
+      }
+    }
 	})
 };
 
